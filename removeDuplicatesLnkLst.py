627373cd0c8fea7b0598f5e5ef4ccf39f89b2d3f
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-05-28 18:22:56 +0600

    Update removeDuplicatesLnkLst.py

--- removeDuplicatesLnkLst.py
+++ removeDuplicatesLnkLst.py
@@ -15,10 +15,11 @@ Brute-force:
 '''
 class Node:
   def __init__():
-    data = ...
-    next = ...
+    self.data = ...
+    self.next = ...
 '''
-    
+
+# This solution takes extra O(n) space in time complexity, so not the most optimal
 def removeDuplicatesSinglyLL(lnk_lst):
   prev = lnk_lst.header
   curr = lnk_lst.header.next
@@ -32,8 +33,8 @@ def removeDuplicatesSinglyLL(lnk_lst):
     else:
       # disconnect the node
       prev.next = curr.next
-      curr = prev.next
       curr.next = None
+      curr = prev.next
 
 '''
 Test Case:
@@ -41,4 +42,27 @@ h -> 1 -> 2 | -> 2 | -> 3 -> t
 prev: h, 1, 2, 2, 3
 curr: 1, 2, 2, 3, t
 uniqueNodes = {1: Node<1>, 2: Node<2>, 3: Node<3>}
+'''
+
+# More optimal solution
+def removeDuplicatesSinglyLL(lnk_lst):
+  curr = lnk_lst.header.next
+  # iterate, O(n)
+  while curr.data is not None:
+    # check and move forward
+    if curr.data == curr.next.data:
+      # disconnect the node
+      new_node = curr.next.next
+      curr.next = None
+      curr.next = new_node
+    else:
+      curr = curr.next
+      
+'''
+Test Case:
+
+h -> 1 -> 2 -> 2 -> 3 -> t
+
+1 -> 2 -> ... -> 3 ->
+
 '''
