68455f0c67e3616a0f688e392f4f75b806331fd5
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-05-28 18:49:23 +0600

    Create reverseKelemsQueue.py

---
+++ reverseKelemsQueue.py
@@ -0,0 +1,6 @@
+'''
+Given an integer K and a queue of integers, we need to reverse the order of the first K elements of the queue, leaving the other elements in the same relative order.
+'''
+
+def reverseQueue(queue):
+  pass
