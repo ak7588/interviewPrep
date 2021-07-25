8ecdec3b366560154767ab12f07234791e0e08eb
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-07-25 14:39:32 +0600

    Create longestBalancedParenthesis.py

---
+++ longestBalancedParenthesis.py
@@ -0,0 +1,32 @@
+'''
+Given a string of parentheses, find the size of the longest contiguous substring of balanced parentheses.
+Parentheses are considered balanced when there is a valid closing parenthesis for an opening one.
+
+Plan:
+1. have two stacks that will keep the parenth
+2. iterate through the string
+  2.1. if '(' found - push to opening stack
+  2.2. if ')' found - push to closing stack
+3. one way to deremine if unbalanced is comparing the lengths of two stacks
+
+(())()()(((()
+
+(((((((( - opening
+))))) - closing
+
+
+'''
+
+
+def longest_balanced(string):
+  longestBalancedLength = 0
+  opening = []
+  closing = []
+  for char in string:
+    if char == '(':
+      opening.append(char)
+    elif char == ')':
+      closing.append(char)
+    if len(opening) == len(closing) and len(opening) != 0:
+      longestBalancedLength = len(opening) + len(closing)
+  return longestBalancedLength
