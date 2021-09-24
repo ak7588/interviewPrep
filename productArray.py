10dce7a5a4362c301853cc0cf6da716e9d7d0fa9
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-09-24 13:45:55 +0600

    Update productArray.py

--- productArray.py
+++ productArray.py
@@ -3,3 +3,14 @@ Given an integer array nums, return an array answer such that answer[i] is equal
 
 array slicing from 0...n
 """
+
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    output = [1] * len(nums)
+    L = len(output)
+    for i in range(1,L):
+        output[i] = output[i-1] * nums[i-1]
+    r = 1
+    for i in reversed(range(L)):
+        output[i] = output[i] * r
+        r = r * nums[i]
+    return output
